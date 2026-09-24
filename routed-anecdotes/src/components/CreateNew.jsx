aa5713450357fdcef54f90a9016bab21e882dc48
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2026-09-24 07:31:40 +0600

    Part 1 completed

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useAnecdotes, useField } from "../hooks"
 
-const CreateNew = ({ addNew }) => {
-  const [content, setContent] = useState("")
-  const [author, setAuthor] = useState("")
-  const [info, setInfo] = useState("")
+const CreateNew = () => {
+  const { addAnecdote } = useAnecdotes()
+
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    addNew({ content, author, info, votes: 0 })
+    addAnecdote({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
     navigate("/")
   }
 
@@ -19,29 +26,22 @@ const CreateNew = ({ addNew }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button type="button" onClick={()=>{
+          content.reset(),
+          author.reset(),
+          info.reset()
+        }}>reset</button>
       </form>
     </div>
   )
--- routed-anecdotes/src/services/anecdotes.js
+++ routed-anecdotes/src/services/anecdotes.js
@@ -4,7 +4,7 @@ const getAll = async () => {
   const response = await fetch(baseUrl)
 
   if (!response.ok) {
-    throw new Error('Failed to fetch notes')
+    throw new Error('Failed to fetch anecdotes')
   }
 
   return await response.json()
@@ -16,12 +16,22 @@ const createNew = async (object) => {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(object),
   })
-  
+
   if (!response.ok) {
     throw new Error('Failed to create note')
   }
-  
+
   return await response.json()
 }
 
-export default { getAll, createNew }
+const remove = async (id) => {
+  const response = await fetch(`${baseUrl}/${id}`, {
+    method: 'DELETE'
+  })
+
+  if (!response.ok) {
+    throw new Error('Failed to delete anecdote')
+  }
+}
+
+export default { getAll, createNew, remove }
